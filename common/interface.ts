d901b828639c3550c03c310c64d5de005abdd39e
honghyeong <scarletgold0601@gmail.com>
2022-09-12 20:46:34 +0900

    init project

---
+++ common/interface.ts
@@ -0,0 +1,5 @@
+export class EnvVars {
+  DB_URL: string;
+  DB_SYNC: string;
+  JWT_SECRET: string;
+}
---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvVars } from 'common/interface';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService<EnvVars>) => ({
+        type: 'postgres',
+        url: config.get('DB_URL'),
+        synchronize: config.get('DB_SYNC') || false,
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
